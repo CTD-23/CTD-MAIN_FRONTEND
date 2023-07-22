bac6a5b65204db155a203c217087d74b7255557c
Maheshwar098 <107045447+Maheshwar098@users.noreply.github.com>
2023-07-22 19:03:16 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/NavBar";
+import Footer from "./components/Footer/Footer";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import ContactUs from "./pages/ContactUs/ContactUs";
+import Events from "./pages/Events/Events";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import OurTeam from "./pages/OurTeam/OurTeam";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/AboutUs" element={<AboutUs />} />
+          <Route exact path="/ContactUs" element={<ContactUs />} />
+          <Route exact path="/Events" element={<Events />} />
+          <Route exact path="/Login" element={<Login />} />
+          <Route exact path="/Register" element={<Register />} />
+          <Route exact path="/OurTeam" element={<OurTeam />} />
+        </Routes>
+      </div>
+      <div>
+        <Footer/>
+      </div>
     </div>
   );
 }
