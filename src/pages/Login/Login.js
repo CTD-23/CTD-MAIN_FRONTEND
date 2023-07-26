260e63bb3d7e12137803926e0cbb295ea55512d0
pranav <pranavbhiungade25@gmail.com>
2023-07-26 22:59:35 +0530

    fixed css conflicts

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,49 +1,79 @@
 import React from 'react'
 import './login.css'
+import axios from 'axios';
+import { useState } from 'react';
 
 const Login = () => {
-  return (
+
+    const [formData, setFormData] = useState({
+        Username: "",
+        password: "",
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData, [name]: value,
+        });
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //    console.log(formData,"<-This is FormData");
+        const loginPayload = {
+            Username: formData.Username,
+            password: formData.password,
+        }
+        axios.post("/login", loginPayload)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(err => console.log(err));
+    }
+
+
+
+return (
     <div>
 
-<div  className="mt-sm-5 mt-0">
+        <div className="mt-sm-5 mt-0">
 
-<div className="container cont text-white p-4 p-sm-4 mt-sm-0 log-style" >
+            <div className="container cont text-white p-4 p-sm-4 mt-sm-0 log-style" >
 
-    <form>
-        <div className="title mb-4 text-center">
-            <h1>Sign in</h1>
-            <small>We are happy to have you back.</small>
-            
-        </div>
-        <div className="input-group mb-4">
-            <input type="text" className="form-control form-control-lg fs-6 input-style" placeholder="&#xF32C;  Email address" />
-    </div>
-    <div className="input-group mb-3">
-        <input type="password" className="form-control form-control-lg fs-6 input-style" placeholder="&#xF44E;  Password" />
-    </div>
-      
-        <div className="input-group mb-3 d-flex justify-content-between">
-            <div>
-                 <small><a href="#" className="forgot hover-link">
-                     Forgot Password?
-                 </a></small>
+                <form>
+                    <div className="title mb-4 text-center">
+                        <h1>Sign in</h1>
+                        <small>We are happy to have you back.</small>
+
+                    </div>
+                    <div className="input-group mb-4">
+                        <input type="text" className="form-control form-control-lg fs-6 input-style" placeholder="&#xF32C;  Email address" name="email" value={formData.email} onChange={handleInputChange} />
+                    </div>
+                    <div className="input-group mb-3">
+                        <input type="password" className="form-control form-control-lg fs-6 input-style" placeholder="&#xF44E;  Password" name="password" value={formData.password} onChange={handleInputChange} />
+                    </div>
+
+                    <div className="input-group mb-3 d-flex justify-content-between">
+                        <div>
+                            <small><a href="#" className="forgot hover-link">
+                                Forgot Password?
+                            </a></small>
+                        </div>
+                    </div>
+                    <div className="input-group mb-4">
+                        <button className="btn btn-lg w-100 fs-6" type="submit" onClick={handleSubmit}>Login</button>
+                    </div>
+                    <div className="row forgot">
+                        <small>Don't have account? <a href="#" className="forgot-signup hover-link">Sign Up</a></small>
+                    </div>
+                </form>
+
             </div>
-         </div>
-         <div className="input-group mb-4">
-             <button className="btn btn-lg w-100 fs-6">Login</button>
-         </div>
-         <div className="row forgot">
-             <small>Don't have account? <a href="#" className="forgot-signup hover-link">Sign Up</a></small>
-         </div>
-      </form>
-
-</div>
 
 
 
-</div>
-</div>
-  );
+        </div>
+    </div>
+);
 }
 
 export default Login
